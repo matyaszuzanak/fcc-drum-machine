3364c9a5aceb2b612d50a9b08db7b17d7efe0a4a
Matyáš Zuzaňák <matyas.zuzanak@Matyas-MacBook-Air.local>
2019-06-25 11:52:14 +0200

    enter

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import './App.css';
 
 const sounds = [{
-  keyCode: 81, keyTrigger: 'Q', id: 'Heater-1', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  keyCode: 81, keyTrigger: 'Q', id: 'HEATER-1', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
 }, {
-  keyCode: 87, keyTrigger: 'W', id: 'Heater-2', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  keyCode: 87, keyTrigger: 'W', id: 'HEATER-2', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
 }, {
-  keyCode: 69, keyTrigger: 'E', id: 'Heater-3', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  keyCode: 69, keyTrigger: 'E', id: 'HEATER-3', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
 }, {
-  keyCode: 65, keyTrigger: 'A', id: 'Heater-4', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  keyCode: 65, keyTrigger: 'A', id: 'HEATER-4', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
 }, {
-  keyCode: 83, keyTrigger: 'S', id: 'Clap', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  keyCode: 83, keyTrigger: 'S', id: 'CLAP', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
 }, {
-  keyCode: 68, keyTrigger: 'D', id: 'Open-HH', src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  keyCode: 68, keyTrigger: 'D', id: 'OPEN-HH', src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
 }, {
-  keyCode: 90, keyTrigger: 'Z', id: "Kick-n'-Hat", src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  keyCode: 90, keyTrigger: 'Z', id: "KICK-N'-HAT", src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
 }, {
-  keyCode: 88, keyTrigger: 'X', id: 'Kick', src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  keyCode: 88, keyTrigger: 'X', id: 'KICK', src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
 }, {
-  keyCode: 67, keyTrigger: 'C', id: 'Closed-HH', src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  keyCode: 67, keyTrigger: 'C', id: 'CLOSED-HH', src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
 },
 ];
 
@@ -61,7 +61,7 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      display: "Click or Press a Button"
+      display: "CLICK OR PRESS A BUTTON"
     }
   }
 
@@ -70,7 +70,7 @@ class App extends React.Component {
   render() {
     return (
       <div id="drum-machine">
-        <div class="d-flex p-2">
+        <div class="d-flex align-items-center">
           <div id="display">{this.state.display}</div>
           <div id="drum-pads">
             {sounds.map(s => (<DrumPad
